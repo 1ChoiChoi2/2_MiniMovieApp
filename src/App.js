0f7cd6e19e5c8daf299c08505eb943bb95513dfe
1ChoiChoi2 <khanhduc.nguyenhoang12@gmail.com>
2023-10-21 10:01:42 +0700

    Fetching Data based on Search function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 
 function App() {
+  const [searchMovieTerm, setSearchMovieTerm] = useState("");
+
+  // Fetch Movie based on search
+  const fetchMovies = async (term) => {
+    const { data } = await axios.get(
+      `https://www.omdbapi.com/?apikey=4715258e&s=${term}`
+    );
+    const moviesData = data.Search;
+    
+    console.log(moviesData || "Not found any movies");
+  };
+
+  // Track the Movie Search Term
+  const handleMovieSearch = (movieTerm) => {
+    setSearchMovieTerm(movieTerm);
+  };
+
+  // Search Movies by clicking and "Enter" key
+  const searchMovie = () => {
+    fetchMovies(searchMovieTerm);
+  };
+
   return (
     <div className="App">
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              searchMovie={searchMovie}
+              handleMovieSearch={handleMovieSearch}
+            />
+          }
+        />
       </Routes>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,34 @@
 import React from "react";
-import UndrawMovie from '../assets/movieUndraw.jpg';
-import '../styles/Home.css'
+import UndrawMovie from "../assets/movieUndraw.jpg";
+import "../styles/Home.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Home = () => {
+const Home = ({ searchMovie, handleMovieSearch }) => {
   return (
     <section id="home">
       <header>
         <div className="header__container">
           <div className="header__description">
-            <h1 className="purple">Vietnam's most awarded online movies watching platform</h1>
+            <h1 className="purple">
+              Vietnam's most awarded online movies watching platform
+            </h1>
             <h2>
               Find your favorite movie with <span className="purple">Choi</span>
             </h2>
             <div className="header__movie--search">
-              <input type="text" placeholder="Search by Movie's Title..."/>
-              <button className="header__movie--search__icon">
-                <FontAwesomeIcon icon='fa-magnifying-glass'/>
+              <input
+                onChange={(e) => handleMovieSearch(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && searchMovie()}
+                type="text"
+                placeholder="Search by Movie's Title..."
+              />
+              <button onClick={searchMovie} className="header__movie--search__icon">
+                <FontAwesomeIcon icon="fa-magnifying-glass" />
               </button>
             </div>
           </div>
           <figure className="header__img--wrapper">
-            <img src={UndrawMovie} alt="movie watching"/>
+            <img src={UndrawMovie} alt="movie watching" />
           </figure>
         </div>
       </header>
